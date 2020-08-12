cb3ea557745c4b5446c3e552a4c42e7f73ebe71f
macgors <macgors@gmail.com>
2020-08-12 08:54:20 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  currentWeatherAPIUrl : 'https://api.openweathermap.org/data/2.5/weather?id=3081368&appid=b6827704beef83a1d7119a562241f41d&lang=pl&units=metric',
+  forecastWeatherAPIUrl : 'https://api.openweathermap.org/data/2.5/onecall?lat=51.107883&lon=17.038538&exclude=hourly,minutely,current&appid=b6827704beef83a1d7119a562241f41d&lang=pl&units=metric',
 };
 
 /*
